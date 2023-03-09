00e033d72003f6be0d21b67175958f5c17d77d94
itonttu <42998227+itonttu@users.noreply.github.com>
2023-03-09 10:16:45 +0900

    1st commit

---
+++ foreach.php
@@ -0,0 +1,13 @@
+<?php
+$colors =["red","blue","pink"];
+
+foreach($colors as $value):
+    echo"$value";
+endforeach;
+
+?>
+<ul>
+    <?php foreach ($colors as $value): ?>
+    <li><?php echo "$value"; ?></li>
+    <?php endforeach; ?>
+</ul>
